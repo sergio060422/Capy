14b7a44e2ee3e84252eb4e61008d154e1b673d5b
sergio060422 <mr.sergiolooolll@gmail.com>
2024-06-07 05:23:29 -0600

    Actualizar serv.py

--- serv.py
+++ serv.py
@@ -1,6 +1,12 @@
 from flask import *
 web = Flask("web")
 
+def read(filename):
+   file=open(filename,'rb')
+   data=file.read(2**30)
+   file.close()
+   return data
+
 @web.route('/<path:filename>')
 def main(filename):
    if(filename.endswith('.js') or filename.endswith('.ts')):
@@ -13,9 +19,4 @@ def main(filename):
 def main2():
    return read('index.html')
 
-def read(filename):
-   file=open(filename,'rb')
-   data=file.read(2**30)
-   file.close()
-   return data
 web.run("0.0.0.0",80)
